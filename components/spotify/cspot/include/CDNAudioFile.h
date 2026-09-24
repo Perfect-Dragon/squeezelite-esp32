73cfc28886d38851a53be4f319f1f67f117bc68a
Perfect-Dragon <rares.tofan.rt@gmail.com>
2026-09-24 17:45:03 +0200

    http_buffer_size changed to 64, testing to remove network spikes

--- components/spotify/cspot/include/CDNAudioFile.h
+++ components/spotify/cspot/include/CDNAudioFile.h
@@ -57,7 +57,7 @@ class CDNAudioFile {
   const int OPUS_FOOTER_PREFFERED = 1024 * 12;  // 12K should be safe
   const int SEEK_MARGIN_SIZE = 1024 * 4;
 
-  const int HTTP_BUFFER_SIZE = 1024 * 14;
+  const int HTTP_BUFFER_SIZE = 1024 * 64;
   const int SPOTIFY_OPUS_HEADER = 167;
 
   // Used to store opus metadata, speeds up read
